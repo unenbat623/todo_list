84a52bd5f729865da5c36b1c5db373fa63eba8af
unenbat623 <batbaatarunenbat20@gmail.com>
2026-05-05 15:23:51 +0800

    fix download bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icon-512.png" sizes="512x512" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="VibeTodo" />
+      </head>
       <body
         className={`${inter.variable} ${oswald.variable} ${playfair.variable} ${robotoMono.variable} ${manrope.variable} ${montserrat.variable} ${lora.variable} antialiased`}
       >
